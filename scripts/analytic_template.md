f790959eb510084fc6119afcc1bca6e860224180
John Wunder <jwunder@mitre.org>
2019-05-14 15:53:55 -0400

    Migrated analytics to be YAML, generator for markdown

---
+++ scripts/analytic_template.md
@@ -0,0 +1,50 @@
+---
+title: "{{analytic['id']}}: {{analytic['title']}}"
+layout: analytic
+submission_date: {{analytic['submission_date']}}
+information_domain: {{analytic['information_domain']}}
+subtypes: {{analytic['subtypes']|join(', ')}}
+analytic_type: {{analytic['analytic_types']|join(', ')}}
+contributors: {{analytic['contributors']|join(', ')}}
+---
+
+{{analytic['description']}}
+{% if 'references' in analytic %}
+### References
+{{ analytic['references']|join("\n") }}
+{%endif %}
+{% if 'coverage' in analytic %}## ATT&CK Detection
+
+|Technique |Tactic |Level of Coverage |
+|---|---|---|{% for coverage_item in analytic['coverage'] %}
+|[{{techniques[coverage_item['technique']]}}](https://attack.mitre.org/techniques/{{coverage_item['technique']}}/)|{% for tactic in coverage_item['tactics'] %}[{{tactics[tactic]}}](https://attack.mitre.org/tactics/{{tactic}}/){% endfor %}|{{coverage_item['coverage']}}|{% endfor %}
+{% endif %}{% if 'data_model_references' in analytic %}
+## Data Model References
+
+|Object|Action|Field|
+|---|---|---|
+{% for dmr in analytic['data_model_references'] %}|[{{dmr[0]}}](../data_model/{{dmr[0]}}) | [{{dmr[1]}}](../data_model/{{dmr[0]}}#{{dmr[1]}}) | [{{dmr[2]}}](../data_model/{{dmr[0]}}#{{dmr[2]}}) |
+{% endfor %}{% endif %}
+{% if 'implementations' in analytic %}
+## Implementations
+{% for impl in analytic['implementations'] %}
+### {{impl['type']|capitalize}}{% if 'data_model' in impl %} ({{impl['data_model']}}){% endif %}
+{% if 'description' in impl %}
+{{impl['description']}}
+{% endif %}
+```
+{{impl['code']}}
+```
+{% endfor %}{% endif %}
+{% if 'unit_tests' in analytic %}
+## Unit Tests
+{% for ut in analytic['unit_tests'] %}
+### Test Case {{loop.index}}
+{% if 'configurations' in ut %}
+**Configurations:** {{ut['configurations']|join(', ')}}{% endif %}{% if 'description' in ut %}
+{{ut['description']}}{% endif %}
+{% if 'commands' in ut %}
+```
+{{ut['commands']|join("\n")}}
+```
+{% endif %}{% endfor %}{% endif %}
--- scripts/generate.py
+++ scripts/generate.py
@@ -1,17 +1,72 @@
-"""This script generates the README file from the list of analytics in the analytics directory"""
+"""This script generates the analytics portions of the site from the YAML in the analytics directory.
 
+The following files are updated by this script, and should not be altered manually:
+* docs/analytics/* (all CAR-NNNN-NN-NNN.md files and index.md)
+* docs/data/analytics.json
+
+"""
+
+import json
 import glob
+import yaml
+import requests
+from jinja2 import Template
 from os import path
+import copy
 
-def table_row(filename):
-    txt = open(filename, "r").read()
+ATTACK_URL = "https://raw.githubusercontent.com/mitre/cti/master/enterprise-attack/enterprise-attack.json"
 
-    # Get the title from the first line
-    title = txt.split('\n')[0].replace("# ", "")
-    localfn = path.split(filename)[1]
-    return ["[{}]({})".format(title, localfn)]
+# Get all analytics and load as list of dicts
+analytics_files = glob.glob(path.join(path.dirname(__file__), "..", "analytics", "*.yaml"))
+analytics = [yaml.load(open(analytic_file).read()) for analytic_file in analytics_files]
 
-analytics = glob.glob(path.join(path.dirname(__file__), "..", "analytics", "CAR-*"))
-rows = [["Analytic"], ["---"]] + [table_row(analytic) for analytic in analytics]
-open(path.join(path.dirname(__file__), "..", "analytics", "README"), "w").write("|" + "\n|".join(["|".join(row) for row in rows]))
+# Load ATT&CK content, which is needed to get names for technique IDs
+attack = requests.get(ATTACK_URL).json()
+techniques = {ap['external_references'][0]['external_id']: ap['name'] for ap in attack['objects'] if ap['type'] == 'attack-pattern'}
+tactics = {ap['external_references'][0]['external_id']: ap['name'] for ap in attack['objects'] if ap['type'] == 'x-mitre-tactic'}
 
+# Get the template file for the analytic page. Note that this is a markdown template which will be rendered by GH Pages.
+analytic_template = Template(open('analytic_template.md').read())
+
+# Generate the analytic page for each analytic
+for analytic in analytics:
+
+    # Do a bit of reformatting to make the template less ugly
+    analytic_for_render = copy.deepcopy(analytic)
+    if 'data_model_references' in analytic_for_render:
+        analytic_for_render['data_model_references'] = [dmr.split('/') for dmr in analytic_for_render['data_model_references']]
+
+    # Generate the markdown
+    markdown = analytic_template.render(analytic=analytic_for_render, tactics=tactics, techniques=techniques)
+
+    # Save to the analytic page
+    open('../docs/analytics/{}.md'.format(analytic['id']), 'w').write(markdown)
+
+# Generate the index.md file
+index_content = """---
+title: "Analytics"
+---
+
+|Analytic|ATT&CK Techniques|
+|---|---|
+"""
+
+for analytic in sorted(analytics, key = lambda k: k['id']):
+    coverage = ""
+    if 'coverage' in analytic and len(analytic['coverage']) > 0:
+        coverage = ", ".join(["[{}](https://attack.mitre.org/techniques/{}/)".format(techniques[coverage['technique']], coverage['technique']) for coverage in analytic['coverage']])
+    index_content += "|{}: {}|{}\n".format(analytic['id'], analytic['title'], coverage)
+
+open('../docs/analytics/index.md', 'w').write(index_content)
+
+# Generate analytics.json
+analytics = [
+    {
+        'shortName': analytic['title'],
+        'name': analytic['id'],
+        'fields': analytic['data_model_references'] if 'data_model_references' in analytic else [],
+        'attack': [{'tactics': [tactics[t] for t in coverage['tactics']], 'technique': 'Technique/{}'.format(coverage['technique']), 'coverage': coverage['coverage']} for coverage in analytic['coverage']] if 'coverage' in analytic else []
+    } for analytic in analytics
+]
+
+open('../docs/data/analytics.json', 'w').write(json.dumps({'analytics': analytics}))
