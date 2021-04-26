71f01253c33d9ec6bce58236f464bb7678ed21e4
divious1 <josehelps@gmail.com>
2021-04-26 10:13:30 -0400

    processing detections

--- scripts/splunk_security_content_to_car.py
+++ scripts/splunk_security_content_to_car.py
@@ -1,11 +1,59 @@
 import argparse
+import yaml
+from pyattck import Attck
 
-def generate_car_analytics(DETECTION_PATH, OUTPUT_DIR, ANALYTICS_TEMPLATE_PATH, attack, messages, VERBOSE):
+def mitre_attack_object(technique, attack):
+    mitre_attack = dict()
+    mitre_attack['technique_id'] = technique.id
+    mitre_attack['technique'] = technique.name
+
+    # process tactics
+    tactics = []
+    for tactic in technique.tactics:
+        tactics.append(tactic.name)
+    mitre_attack['tactic'] = tactics
+
+    return mitre_attack
+    
+def get_mitre_enrichment_new(attack, mitre_attack_id):
+    for technique in attack.enterprise.techniques:
+        apt_groups = []
+        if '.' in mitre_attack_id:
+            for subtechnique in technique.subtechniques:
+                if mitre_attack_id == subtechnique.id:
+                    mitre_attack = mitre_attack_object(subtechnique, attack)
+                    return mitre_attack
+
+        elif mitre_attack_id == technique.id:
+            mitre_attack = mitre_attack_object(technique, attack)
+            return mitre_attack
+    return []
+
+def generate_car_analytics(DETECTION_PATH, OUTPUT_DIR, ANALYTICS_TEMPLATE_PATH, attack, VERBOSE):
 
     if VERBOSE:
         print("reading splunk security content detection: {0}".format(DETECTION_PATH))
 
     detection_yaml = dict()
+    with open(DETECTION_PATH, 'r') as stream:
+        try:
+            object = list(yaml.safe_load_all(stream))[0]
+        except yaml.YAMLError as exc:
+            print(exc)
+            print("Error reading {0}".format(manifest_file))
+            sys.exit(1)
+
+    detection_yaml = object
+
+    # enrich the mitre object
+    mitre_attacks = []
+    if 'mitre_attack_id' in detection_yaml['tags']:
+        for mitre_technique_id in detection_yaml['tags']['mitre_attack_id']:
+            mitre_attack = get_mitre_enrichment_new(attack, mitre_technique_id)
+            mitre_attacks.append(mitre_attack)
+        detection_yaml['mitre_attacks'] = mitre_attacks
+
+    print(detection_yaml)
 
 if __name__ == "__main__":
 
@@ -27,5 +75,5 @@ if __name__ == "__main__":
         print("getting mitre enrichment data from cti")
     attack = Attck()
 
-    generate_car_analytics(DETECTION_PATH, OUTPUT_DIR, ANALYTICS_TEMPLATE_PATH, attack, messages, VERBOSE)
+    generate_car_analytics(DETECTION_PATH, OUTPUT_DIR, ANALYTICS_TEMPLATE_PATH, attack, VERBOSE)
     print("finished successfully!")
